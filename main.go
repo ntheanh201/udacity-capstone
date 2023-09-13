904c96ba54199b562f6f341cb2ffe4d2b91c9b49
The Anh Nguyen <ntheanh201@gmail.com>
2023-09-13 11:34:43 +0700

    remove Gin

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	err := r.Run()
-	if err != nil {
-		return
-	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	fmt.Println("Hello world!")
 }
