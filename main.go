cff5eed4894f2c7727eaba07b7f425f75910faa2
The Anh Nguyen <ntheanh201@gmail.com>
2023-09-13 11:29:07 +0700

    handle error gin

--- main.go
+++ main.go
@@ -13,5 +13,8 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	err := r.Run()
+	if err != nil {
+		return
+	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
