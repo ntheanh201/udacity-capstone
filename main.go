04c0ee914df657c0205881afda4d74c814b55059
The Anh Nguyen <ntheanh201@gmail.com>
2023-09-13 13:58:33 +0700

    update code & add k8s manifests

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	_, err := io.WriteString(w, "Hello World!\n")
+	if err != nil {
+		return
+	}
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	http.HandleFunc("/", getRoot)
+
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
